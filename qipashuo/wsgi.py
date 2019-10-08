00537bdba30d3b05728685e8080ecfb7f92107ad
Jack Li Shufan <lsl42@126.com>
2019-10-08 03:24:28 -0700

    new

--- qipashuo/wsgi.py
+++ qipashuo/wsgi.py
@@ -16,5 +16,5 @@ from django.core.wsgi import get_wsgi_application
 # application = get_wsgi_application()
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qipashuo.settings")
 application = Cling(get_wsgi_application())
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/local/Application/Finance/UCB/Fall 2019/BCSSADebating/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
